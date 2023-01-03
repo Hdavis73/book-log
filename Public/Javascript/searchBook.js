8333396403bb7fb8f0cdc47b254ae5f41776ffa7
Heather <lilpinkrose02@gmail.com>
2023-01-02 20:06:16 -0600

    started adding a default display of bestselling books using NYTimes API and Google books API. Still working out the kinks

--- Public/Javascript/searchBook.js
+++ Public/Javascript/searchBook.js
@@ -1,6 +1,12 @@
-    let googleBooksUrl = 'https://www.googleapis.com/books/v1/volumes?q=ugly+love&maxResults=20'
+    const searchBtn = document.querySelector('.search-book-btn')
+    const searchInput = document.querySelector('.search-book-input')
+    const bestSellersUrl = 'https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&&api-key=fdXgMoy5fKdWsnKYYNWbUpbrQ99O9xJe'
+    let searchByIsbn = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
+    let query
     let searchResults = []
+    let bestSellerIsbn = []
     
+    // create necessary elements for layout
     function createGroup(){
         const div = document.createElement('div')
         const img = document.createElement('img')
@@ -16,44 +22,83 @@
         h3.classList.add('book-title')
     }
 
-    fetch(googleBooksUrl)
-        .then(res => res.json())
-        .then(data => {
-            
-            for(let i = 0; i < data.items.length; i++){
-                console.log(data.items[i].volumeInfo.title)
+    //fetch the NYTimes Bestseller API and add the ISBN numbers of each to an array to be searched with google books api later
+    function fetchBestSellerIsbn(url){
 
-                if(data.items[i].volumeInfo.imageLinks && !data.items[i].volumeInfo.title.toLowerCase().includes('summary') && !data.items[i].volumeInfo.title.toLowerCase().includes('ebook')) searchResults.push(data.items[i])
+     fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
 
-                // searchResults.push(data.items[i])
+                for(let i = 0; i < data.results.length; i++){
+                    bestSellerIsbn.push(data.results[i].isbns[0])
+                }
 
+                console.log(bestSellerIsbn)
+            })
+
+    }
+
+    //fetch books by ISBN with Google Books API so more information can be displayed 
+    function fetchGoogleBooksByIsbn(url){
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    //on search btn click, removing previous search results, saving query as a variable and calling a funciton to display new results
+    searchBtn.addEventListener('click', () => {
+        query = searchInput.value
+        let googleBooksUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=20`
+        const bookContainer = document.querySelector('.book-group')
+        let books = Array.from(document.querySelectorAll('.single-book'))
+
+        console.log(query)
+
+        for(let i = books.length-1; i >= 0; i--){
+            bookContainer.removeChild(books[i])
+            searchResults = []
+        }
+        
+        fetchGoogleBooks(googleBooksUrl)
+
+    })
+
+    //display search results, parameters on what will be displayed based on title is because google has a variety of results that are not relevent to this apps function such as summarys, collections and ebook only editions
+    function fetchGoogleBooks(url){
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                
+                for(let i = 0; i < data.items.length; i++){
+                    // console.log(data.items[i].volumeInfo.title)
+    
+                    if(data.items[i].volumeInfo.imageLinks && 
+                        !data.items[i].volumeInfo.title.toLowerCase().includes('summary') && 
+                        !data.items[i].volumeInfo.title.toLowerCase().includes('ebook') &&
+                        !data.items[i].volumeInfo.title.toLowerCase().includes('collection')) searchResults.push(data.items[i])
+              
+                    }
+    
+                console.log(data)
+    
+                for(let i = 0; i < searchResults.length; i++){
+                    createGroup()
+                }
+    
+                console.log(searchResults)
+    
+             let bookDivs = Array.from(document.querySelectorAll('.single-book'))
+    
+                 for(let i = 0; i < bookDivs.length; i++){
+                      bookDivs[i].querySelector('img').src = searchResults[i].volumeInfo.imageLinks.thumbnail
+                     bookDivs[i].querySelector('h3').innerText = searchResults[i].volumeInfo.title
+                 }
+    
+                })
             }
 
-            console.log(data)
-            // console.log(data.items.volumeInfo.title)
 
-            for(let i = 0; i < searchResults.length; i++){
-                createGroup()
-            }
-
-            console.log(searchResults)
-
-            let bookDivs = Array.from(document.querySelectorAll('.single-book'))
-
-            for(let i = 0; i < bookDivs.length; i++){
-
-                // if(!searchResults[i].volumeInfo.imageLinks.thumbnail) searchResults.splice(searchResults[i], 1)
-
-                bookDivs[i].querySelector('img').src = searchResults[i].volumeInfo.imageLinks.thumbnail
-                bookDivs[i].querySelector('h3').innerText = searchResults[i].volumeInfo.title
-            }
-
-        })
-
-
-
-    // for(let i = 0; i < )
-
-
-
-
+    fetchBestSellerIsbn(bestSellersUrl)
