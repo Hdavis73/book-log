e27978ffb3eccb129d9924165a09da73ec1ac1c3
Heather <lilpinkrose02@gmail.com>
2023-01-04 22:24:36 -0600

    added some more effects to rating system, trying to add a grow on hover without shifting other elements

--- Public/Javascript/star-rating.js
+++ Public/Javascript/star-rating.js
@@ -1,26 +1,35 @@
-console.log('working')
+console.log("working");
 
-let stars = Array.from(document.querySelectorAll('.star'))
+let stars = Array.from(document.querySelectorAll(".star"));
 
-stars.forEach(star => {
+stars.forEach((star) => {
+  star.addEventListener("click", () => {
+    if (star.classList.contains("active")) {
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove("active");
+      }
+    } else {
+      for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove("active");
+      }
 
-    star.addEventListener('click', () => {
+      let starRating = stars.slice(0, stars.indexOf(star) + 1);
 
-        for(let i = 0; i < stars.length; i++){
-            stars[i].classList.remove('active')
-        }
+      for (let i = 0; i < starRating.length; i++) {
+        starRating[i].classList.add("active");
+      }
+    }
+  });
 
-        let starRating = stars.slice(0,stars.indexOf(star)+1)
-        
-        for(let i = 0; i < starRating.length; i++){
-            starRating[i].classList.add('active')
-        }
-    })
+  star.addEventListener('mouseover', () => {
+    console.log('mouse is over')
 
-    // function activateStars(){
-    //     for(let i = 0; i < num.length; i++){
-    //         stars[i].classList.add('active')
-    //     }
-    // }
-})
+        star.classList.add('grow')
+  })
+
+  star.addEventListener('mouseleave', () => {
+    star.classList.remove('grow')
+  })
+});
+
 
